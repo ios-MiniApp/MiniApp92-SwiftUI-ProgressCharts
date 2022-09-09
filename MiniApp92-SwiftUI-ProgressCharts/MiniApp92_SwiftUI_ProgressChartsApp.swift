dc51df1e072c34d4c8956a3ace796ea4006e1f12
Kota <maedakoutaa@gmail.com>
2022-09-09 15:07:03 +0900

    Initial Commit

---
+++ MiniApp92-SwiftUI-ProgressCharts/MiniApp92_SwiftUI_ProgressChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp92_SwiftUI_ProgressChartsApp.swift
+//  MiniApp92-SwiftUI-ProgressCharts
+//
+//  Created by 前田航汰 on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp92_SwiftUI_ProgressChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
